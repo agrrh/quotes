b840b2db0f6a355103dabbf33033a2f4aa7491cf
Kirill K <kovalev.kirill.a@gmail.com>
2022-08-19 23:03:03 +0300

    feat: add frontend blueprints

---
+++ frontend/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Header from './lib/Header.svelte'
+  import Main from './lib/Main.svelte'
+  import Footer from './lib/Footer.svelte'
+</script>
+
+<Header />
+
+<Main />
+
+<Footer />
