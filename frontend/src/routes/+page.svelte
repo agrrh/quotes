fb8473fd48c32bdb556c258b8676ccf3e620d4aa
Kirill K <kovalev.kirill.a@gmail.com>
2023-01-16 02:58:55 +0300

    refactor(frontend): refresh

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<script>
+  import Header from '../lib/Header.svelte'
+  import Main from '../lib/Main.svelte'
+  import Footer from '../lib/Footer.svelte'
+</script>
+
+<Header />
+
+<Main />
+
+<Footer />
